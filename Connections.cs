538efb5b77a644371a9d88e8547650abf9bac712
Max Matveenko <supmaks8653@gmail.com>
2021-11-01 19:24:18 +0300

    fully rewritten map collection and tile passabilty

---
+++ Connections.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace connections
+{
+    public struct Connection
+    {
+        public int cx, cy, id;
+        public Connection(int x, int y, int id)
+        {
+            cx = x;
+            cy = y;
+            this.id = id;
+        }
+    }
+}
--- MapCollector.cs
+++ MapCollector.cs
@@ -1,29 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using mapsolvers;
+using connections;
 
-namespace Roguelike
-{
-    public struct Connection
-    {
-        public int cx, cy, id;
-        public Connection(int x, int y, int id)
-        {
-            cx = x;
-            cy = y;
-            this.id = id;
-        }
-    }
-
+namespace mapcollector {
     public class MapCollector {
 
-        List<Map> baseMaps;
+        List<Map> baseMaps = new List<Map>();
+        string[] workingOnMap;
         string[] mapPaths = { "../../../main.map", "../../../boss.map" };
         public MapCollector()
         {
             for(int i = 0; i < mapPaths.Length; i++)
             {
-                System.IO.File.ReadAllLines(mapPaths[i]);
+                workingOnMap = System.IO.File.ReadAllLines(mapPaths[i]);
+                baseMaps.Add(new Map(i, workingOnMap));
             }
         }
         struct Map
@@ -31,25 +23,17 @@ namespace Roguelike
             int MapID;
             string[] textMap;
             string[] drawnMap;
-            List<int> connectionX;
-            List<int> connectionY;
-            List<int> connectionID;
-            Map(int mi, string[] tm)
+            List<Connection> connections;
+            bool[,] standable;
+            public Map(int mi, string[] tm)
             {
-                connectionX = new List<int>();
-                connectionY = new List<int>();
-                connectionID = new List<int>();
-                Solver solver = new Solver();
+                connections = new List<Connection>();
+                MapSolver solver = new MapSolver();
                 MapID = mi;
                 textMap = tm;
-                List<Connection> copy = solver.ConnectionSolver(tm);
-                for (int i = 0; i < copy.Count; i++)
-                {
-                    connectionX.Add(copy[i].cx);
-                    connectionY.Add(copy[i].cy);
-                    connectionID.Add(copy[i].id);
-                }
-                drawnMap = solver.SolveDrawnMap(tm);
+                connections = solver.ConnectionSolver(tm);
+                drawnMap = solver.SolverDrawnMap(tm);
+                standable = solver.SolverStandable(tm, "#~ ");
             }
         }
     }
@@ -63,44 +47,4 @@ namespace Roguelike
             }
         }
     }
-    class Solver
-    {
-        public List<Connection> ConnectionSolver(string[] map)
-        {
-            int index = 0;
-            string numbers = "0123456789";
-            List<Connection> result = new List<Connection>();
-            for (int i = 0; i < map.Length; i++)
-            {
-                for (int j = 0; j < map[i].Length; j++)
-                {
-                    index = numbers.IndexOf(map[i][j]);
-                    if (index >= 0)
-                    {
-                        result.Add(new Connection(i, j, index));
-                    }
-                }
-            }
-            return result;
-        }
-        public string[] SolveDrawnMap(string[] map)
-        {
-            string[] result = map;
-            string numbers = "1234567890";
-            for (int i = 0; i < map.Length; i++)
-            {
-                for (int j = 0; j < map[i].Length; j++)
-                {
-                    if (numbers.IndexOf(map[i][j]) >= 0)
-                    {
-                        map[i].ToCharArray()[j] = '#';
-                    }
-                }
-            }
-            return result;
-        }
-        
-    }
-
-    
 }
