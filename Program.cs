94ebb6b2c9f64706777e9b43acddc41bae229783
gwyndol1ne <Dragon>
2021-11-02 20:05:51 +0300

    lol

--- Program.cs
+++ Program.cs
@@ -1,18 +1,58 @@
 ﻿using System;
 using Mapcollector;
 using Screen;
-
+using Menu;
 namespace Roguelike
 {
+    delegate void method();
+   
     class Program
     {
         static void Main(string[] args)
         {
-            MapCollector collector = new MapCollector();
-            Draw screen = new Draw();
-            Player player = new Player();
-            screen.draw(collector.GetCurrentMap(player.GetMapID()));
-            Console.ReadLine();
+           int Playbel =1;
+            void Play()
+            {
+                Console.WriteLine("Начата игра");
+                Playbel =2;
+            }
+           
+            string[] items = { "Новая игра", "Выход" };
+            method[] methods = new method[] { Play, Exit };
+            ConsoleMenu menu = new ConsoleMenu(items);
+            int menuResult;
+            menuResult = menu.PrintMenu();
+            methods[menuResult]();    
+
+            if (Playbel==2)
+            {
+                Console.Clear();
+                string[] Class = { "The Fool", "Magician's Red ", "High Priestess", "Empress ", "Emperor", "Hierophant Green", "Lovers", "Silver Chariot", "Strength", "Hermit Purple", "Wheel of Fortune", "Justice", "Hanged Man", "Death Thirteen", "Yellow Temperance", "Ebony Devil", "Tower of Gray", "Star Platinum", "Dark Blue Moon ", "Sun", "Judgement ", "The World" };
+                
+                ConsoleMenu menu2 = new ConsoleMenu(Class);
+                int menuResult2;
+                menuResult2 = menu2.PrintMenu();
+                Console.Clear();
+                Console.WriteLine("Выбран класс");
+                Console.WriteLine( Class[menuResult2]);
+                Playbel = 3;
+            }
+            System.Threading.Thread.Sleep(500);
+            Console.Clear();
+            if (Playbel==3)
+            {
+                MapCollector collector = new MapCollector();
+                Draw screen = new Draw();
+                Player player = new Player();
+                screen.draw(collector.GetCurrentMap(player.GetMapID()));
+                Console.ReadLine();
+            }
+          
+
+        }
+        static void Exit()
+        {
+            Console.WriteLine("Приложение заканчивает работу!");
         }
     }
 }
