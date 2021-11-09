b3af323986ade825010992ffc40d57898a42ae3f
gwyndol1ne <Dragon>
2021-11-09 18:28:11 +0300

    size
    fhabsdhbfsewfewnfkwenjtarting

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+
+using System.Media;
 namespace Roguelike
 {
     class Program
@@ -9,6 +11,7 @@ namespace Roguelike
         {
             Console.CursorVisible = false;
             Console.SetWindowSize(90, 34);
+            Console.SetBufferSize(90, 34);
             Game.Start();
         }
     }
