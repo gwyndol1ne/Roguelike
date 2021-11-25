0425b1e5b9f9d82b04e4dec514c5769d3d4931d6
Max Matveenko <supmaks8653@gmail.com>
2021-11-25 10:14:29 +0300

    woop

--- Battle.cs
+++ Battle.cs
@@ -49,12 +49,7 @@ namespace Roguelike
                     case 0:
                         GameInterface.DrawBattleInterface(aliveEnemies, friend[0]);
                         target = enemyChoiceMenu.GetChoice(false, false);
-<<<<<<< HEAD
-                        aliveEnemies[target].GetDamaged(friend[0].Stats["damage"][1]+((Player)friend[0]).CountDamage());
-=======
-                        Effect.AddEffect(new EntireEffect(null,new EffectBuff[] { new EffectBuff(2, 10, "defense") }), friend[0]);
                         aliveEnemies[target].GetDamaged(friend[0].Stats["damage"][1]);
->>>>>>> 5f876686369681ada871e2ea1f225607fb6731f9
                         break;
                     case 1:
                         GameInterface.DrawBattleInterface(aliveEnemies, friend[0]);
